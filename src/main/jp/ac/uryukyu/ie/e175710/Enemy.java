b3d221e8dba44408af4df9a6dae8a0ff8a0af94c
momoka shirado <e175710@ie.u-ryukyu.ac.jp>
2017-11-13 18:26:47 +0900

    プログラミング２　レポート３

--- src/main/jp/ac/uryukyu/ie/e175710/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e175710/Enemy.java
@@ -46,10 +46,13 @@ public class Enemy {
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
+        if(isDead() == false){
         int damage = (int)(Math.random() * attack);
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
         hero.wounded(damage);
-    }
+    } else {
+        }
+        }
 
     /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。
